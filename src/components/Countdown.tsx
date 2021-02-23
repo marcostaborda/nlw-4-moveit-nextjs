cbc33c98c12ae8dff81d79549daaeac4c6f7a348
Marcos Taborda <marcos.tabordamail@gmail.com>
2021-02-23 15:29:39 -0300

    feat: add time

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,28 +1,56 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+  const [time, setTime] = useState(23 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  function startCountdown() {
+    setActive(false);
+  }
+
   return (
     <div>
       <div className={styles.countdownContainer}>
         <div>
           <span>
-            2
-        </span>
+            {minuteLeft}
+          </span>
           <span>
-            5
-        </span>
+            {minuteRight}
+          </span>
         </div>
         <span>:</span>
         <div>
           <span>
-            0
-        </span>
+            {secondsLeft}
+          </span>
           <span>
-            0
-        </span>
+            {secondsRight}
+          </span>
         </div>
       </div>
-      <button type="button" className={styles.buttonContainer}>Iniciar novo Ciclo</button>
+      <button
+        type="button"
+        onClick={startCountdown}
+        className={styles.buttonContainer}
+      >
+        Iniciar novo Ciclo
+      </button>
     </div>
   );
 }
